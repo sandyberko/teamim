270ddd555df840d815b29b348f9c445347a00e08
Sandy Berkowitz <sandyberko@outlook.com>
2025-04-07 18:29:39 +0300

    training scripts fixes

---
+++ crates/training/src/bin/search-boxes.rs
@@ -0,0 +1,55 @@
+use clap::Parser;
+use eyre::{Context, OptionExt, ensure};
+use rayon::iter::{ParallelBridge, ParallelIterator};
+use std::{fs, path::Path};
+use teamim::{parse_box_line, tesseract_ext::BoundingBox};
+
+#[derive(clap::Parser)]
+struct Args {
+    term: String,
+}
+
+/// - Add a trailing space to each line
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+
+    let args = Args::parse();
+
+    let src_dir = Path::new("assets/corrected_boxfiles");
+    ensure!(src_dir.is_dir(), "{src_dir:?} is not a dir");
+
+    fs::read_dir(src_dir)?
+        .par_bridge()
+        .map(|entry| {
+            let entry = entry?;
+            ensure!(entry.file_type()?.is_file(), "{entry:?} is not a file");
+            let r = fs::read_to_string(entry.path())?;
+
+            let lines = r.lines().enumerate().map(|(i, line)| {
+                parse_box_line(line)
+                    .wrap_err_with(|| format!("invalid line: {}:{}", entry.path().display(), i + 1))
+            });
+
+            let mut line_i = 1;
+            let mut line: Option<BoundingBox<String>> = None;
+
+            for bx in lines {
+                let bx = bx?;
+                if bx.value == '\t' {
+                    let found = line.ok_or_eyre("empty line")?.value.contains(&args.term);
+                    if found {
+                        println!("{}:{}", entry.path().display(), line_i);
+                    }
+                    line = None;
+                } else if let Some(line) = &mut line {
+                    line.value.push(bx.value);
+                } else {
+                    line = Some(bx.with_value(bx.value.to_string()));
+                    line_i += 1;
+                }
+            }
+            eyre::Ok(())
+        })
+        .collect::<Result<(), _>>()?;
+    Ok(())
+}
--- crates/training/src/bin/verify-boxes.rs
+++ crates/training/src/bin/verify-boxes.rs
@@ -1,5 +1,4 @@
 use eyre::{Context, ensure};
-use rayon::prelude::*;
 use std::{fs, path::Path};
 use teamim::parse_box_line;
 
@@ -10,32 +9,55 @@ fn main() -> eyre::Result<()> {
     let src_dir = Path::new("assets/corrected_boxfiles");
     ensure!(src_dir.is_dir(), "{src_dir:?} is not a dir");
 
-    fs::read_dir(src_dir)?
-        .par_bridge()
-        .map(|entry| {
-            let entry = entry?;
-            ensure!(entry.file_type()?.is_file(), "{entry:?} is not a file");
-            let r = fs::read_to_string(entry.path())?;
+    fs::read_dir(src_dir)?.try_for_each(|entry| {
+        let entry = entry?;
+        ensure!(entry.file_type()?.is_file(), "{entry:?} is not a file");
+        let r = fs::read_to_string(entry.path())?;
 
-            let lines = r.lines().enumerate().map(|(i, line)| {
-                parse_box_line(line)
-                    .wrap_err_with(|| format!("invalid line: {}:{}", entry.path().display(), i + 1))
-            });
+        let lines = r.lines().enumerate().map(|(i, line)| {
+            parse_box_line(line)
+                .wrap_err_with(|| format!("invalid line: {}:{}", entry.path().display(), i + 1))
+        });
 
-            let mut following_space = false;
-            for (i, bx) in lines.enumerate() {
-                (|| {
-                    let line = bx?;
-                    if line.value == ' ' {
-                        ensure!(!following_space, "double space");
-                        following_space = true;
-                    }
-                    eyre::Ok(())
-                })()
-                .wrap_err_with(|| format!("invalid line: {}:{}", entry.path().display(), i + 1))?;
+        let mut following_space = false;
+        let mut line_i = 1;
+        let mut col_i = 1;
+        let mut contains_letter = false;
+
+        for bx in lines {
+            let line = bx?;
+
+            ensure!(
+                !(line.value == ' ' && following_space),
+                "double space at {}:{line_i}:{col_i}",
+                entry.path().display(),
+            );
+
+            following_space = line.value == ' ';
+
+            if line.value == '\t' {
+                ensure!(
+                    contains_letter,
+                    "empty line at {}:{line_i}",
+                    entry.path().display()
+                );
+                contains_letter = false;
+
+                ensure!(
+                    !following_space,
+                    "trailing space before {}:{line_i}:{col_i}",
+                    entry.path().display()
+                );
+
+                line_i += 1;
+                col_i = 1;
+            } else {
+                contains_letter = true;
+
+                col_i += 1;
             }
-            eyre::Ok(())
-        })
-        .collect::<Result<(), _>>()?;
+        }
+        eyre::Ok(())
+    })?;
     Ok(())
 }
