33e86cf964a2d76825fa7a58851c7dae1bfea5d5
Sandy Berkowitz <sandyberko@outlook.com>
2025-04-26 17:56:59 +0300

    train ops

--- crates/training/src/bin/eval.rs
+++ crates/training/src/bin/eval.rs
@@ -22,38 +22,45 @@ fn main() -> eyre::Result<()> {
 
     println!("{}", "Start evaluation...".blue());
     if let Some(path) = args.checkpoint {
-        eval_checkpoint(&path, false).spawn()?;
-    } else {
-        let mut checkpoints = fs::read_dir("assets/training/model")?
-            .map(|entry| {
-                let entry = entry?;
-                Ok(entry.path())
-            })
-            .filter_map(|path| match path {
-                Ok(path) => (path.extension()? == "checkpoint").then_some(eyre::Ok(path)),
-                Err(err) => Some(Err(err)),
-            })
-            .map(|path| {
-                let path = path?;
-                // <model_base>_<char_error>_<learning_iteration>_<training_iteration>.checkpoint
-                let stem =
-                    path.file_stem().ok_or_else(|| eyre::eyre!("no file stem"))?.to_string_lossy();
-                let (_name, train_err, _learn_iter, _train_iter) =
-                    stem.split('_').collect_tuple().ok_or_eyre("invalid file stem")?;
-                eyre::Ok((train_err.parse::<f32>()?, path))
-            })
-            .collect::<Result<Vec<_>, _>>()?;
-        checkpoints.sort_by(|(a, _), (b, _)| b.partial_cmp(a).expect("finite f32"));
 
-        for (err, path) in checkpoints {
-            let res = eval_checkpoint(&path, true)
-                .output()?
-                .stderr
-                .lines()
-                .last()
-                .ok_or_eyre("no result")??;
-            println!("{err:<10} {res}",);
-        }
+        #[cfg(windows)]
+        return Ok(eval_checkpoint(&path, false).spawn()?);
+
+        #[cfg(unix)]
+        return Err(
+            std::os::unix::process::CommandExt::exec(&mut eval_checkpoint(&path, false)).into()
+        );
+    }
+
+    let mut checkpoints = fs::read_dir("assets/training/model")?
+        .map(|entry| {
+            let entry = entry?;
+            Ok(entry.path())
+        })
+        .filter_map(|path| match path {
+            Ok(path) => (path.extension()? == "checkpoint").then_some(eyre::Ok(path)),
+            Err(err) => Some(Err(err)),
+        })
+        .map(|path| {
+            let path = path?;
+            // <model_base>_<char_error>_<learning_iteration>_<training_iteration>.checkpoint
+            let stem =
+                path.file_stem().ok_or_else(|| eyre::eyre!("no file stem"))?.to_string_lossy();
+            let (_name, train_err, _learn_iter, _train_iter) =
+                stem.split('_').collect_tuple().ok_or_eyre("invalid file stem")?;
+            eyre::Ok((train_err.parse::<f32>()?, path))
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    checkpoints.sort_by(|(a, _), (b, _)| b.partial_cmp(a).expect("finite f32"));
+
+    for (err, path) in checkpoints {
+        let res = eval_checkpoint(&path, true)
+            .output()?
+            .stderr
+            .lines()
+            .last()
+            .ok_or_eyre("no result")??;
+        println!("{err:<10} {res}",);
     }
 
     Ok(())
--- crates/training/src/bin/train.rs
+++ crates/training/src/bin/train.rs
@@ -1,9 +1,28 @@
+use clap::Parser;
 use color_eyre::owo_colors::OwoColorize;
-use std::process::Command;
+use std::{os::unix::process::CommandExt, path::PathBuf, process::Command};
 use training::tess_dir;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(short, long)]
+    continue_from: Option<PathBuf>,
+
+    #[clap(short, long)]
+    /// Weight factor for new deltas. (type:double default:0.001)
+    learning_rate: Option<f64>,
+
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(short, long)]
+    /// How many imperfect samples between perfect ones
+    perfect_sample_delay: Option<u32>,
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
+    let args = Args::parse();
 
     #[cfg(windows)]
     {
@@ -16,14 +35,35 @@ fn main() -> eyre::Result<()> {
     }
 
     println!("{}", "Start training...".blue());
-    Command::new(tess_dir().join("lstmtraining"))
-        .args(["--old_traineddata", "assets/langdata/heb/heb.traineddata"])
+    let mut cmd = Command::new(tess_dir().join("lstmtraining"));
+    cmd.args(["--old_traineddata", "assets/langdata/heb/heb.traineddata"])
         .args(["--traineddata", "assets/langdata/stam/stam.traineddata"])
         .args(["--continue_from", "assets/langdata/heb/heb.lstm"])
         .args(["--train_listfile", "assets/training/train.txt"])
         .args(["--eval_listfile", "assets/training/eval.txt"])
-        .args(["--model_output", "assets/training/model/stam"])
-        .spawn()?;
+        .args(["--model_output", "assets/training/model/stam"]);
 
+    if let Some(lr) = args.learning_rate {
+        cmd.arg("--learning_rate").arg(lr.to_string());
+    }
+
+    if let Some(continue_from) = args.continue_from {
+        cmd.arg("--continue_from").arg(continue_from);
+    }
+
+    if let Some(perfect_sample_delay) = args.perfect_sample_delay {
+        cmd.arg("--perfect_sample_delay").arg(perfect_sample_delay.to_string());
+    }
+
+    if args.verbose {
+        cmd.arg("--debug_interval").arg("-1");
+    }
+
+    if cfg!(windows) {
+        cmd.spawn()?;
+    } else {
+        return Err(cmd.exec().into());
+    }
+    
     Ok(())
 }
