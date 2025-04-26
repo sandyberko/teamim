8370b9da3a212a906c6d61e15d817896f9560644
Sandy Berkowitz <sandyberko@outlook.com>
2025-04-26 15:35:00 +0300

    combine_boxes

--- crates/training/src/bin/combine-boxes.rs
+++ crates/training/src/bin/combine-boxes.rs
@@ -1,3 +1,4 @@
+use clap::{Parser, ValueEnum};
 use color_eyre::owo_colors::OwoColorize;
 use core::str;
 use eyre::{OptionExt, bail, ensure};
@@ -7,60 +8,107 @@ use std::{
     fs::{self, File},
     io::{BufWriter, Write, stderr, stdout},
     path::{Path, PathBuf},
-    process::{Command, Output},
+    process::Command,
 };
+use teamim::tesseract_ext::PageSegMode;
 use training::{tess_dir, tessdata_dir};
+
+#[derive(Debug, Clone, Copy, ValueEnum, PartialEq, Eq)]
+enum InputType {
+    Synthetic,
+    Real,
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    inputs: Vec<InputType>,
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
+    let args = Args::parse();
 
     let assets_dir = Path::new("assets");
 
     let out_dir = assets_dir.join("training");
 
-    let files = fs::read_dir(assets_dir.join("corrected_boxfiles"))?
-        .map(|entry| {
-            let entry = entry?;
-            ensure!(entry.file_type()?.is_file(), "{entry:?} is not a file");
-            Ok(entry.path())
-        })
-        .collect::<Result<Vec<_>, _>>()?
-        .into_par_iter()
-        .map(|box_orig_path| {
-            let stem = box_orig_path.file_stem().ok_or_eyre("no stem")?;
+    let files = if args.inputs.contains(&InputType::Synthetic) {
+        fs::read_dir(assets_dir.join("training/images"))?
+            .par_bridge()
+            .filter_map(|entry| match entry {
+                Ok(entry) => {
+                    let path = entry.path();
+                    if path.extension().is_some_and(|ext| ext == "tif") {
+                        Some(Ok(path))
+                    } else {
+                        None
+                    }
+                }
+                Err(err) => Some(Err(err)),
+            })
+            .map(|path| {
+                let path = path?;
+                let stem = path.file_stem().ok_or_eyre("no stem")?;
+                let output_path_stem = out_dir.join("lstmf").join(stem);
+                let log_file = File::create(out_dir.join("logs").join(stem).with_extension("log"))?;
+                generate_lstmf_cmd(&output_path_stem, path, PageSegMode::SingleBlock)
+                    .stderr(log_file.try_clone()?)
+                    .stdout(log_file)
+                    .spawn()?
+                    .wait()?;
+                eyre::Ok(output_path_stem.with_extension("lstmf"))
+            })
+            .collect::<Result<Vec<_>, _>>()?
+    } else {
+        let outs = fs::read_dir(assets_dir.join("corrected_boxfiles"))?
+            .map(|entry| {
+                let entry = entry?;
+                ensure!(entry.file_type()?.is_file(), "{entry:?} is not a file");
+                Ok(entry.path())
+            })
+            .collect::<Result<Vec<_>, _>>()?
+            .into_par_iter()
+            .map(|box_orig_path| {
+                let stem = box_orig_path.file_stem().ok_or_eyre("no stem")?;
 
-            let output_path = out_dir.join("combined").join(stem);
-            let lstmf_path = output_path.with_extension("lstmf");
+                let output_path_stem = out_dir.join("combined").join(stem);
+                let lstmf_path = output_path_stem.with_extension("lstmf");
 
-            let output = if lstmf_path.exists() {
-                None
-            } else {
-                Some(generate_lstmf(stem, &box_orig_path, &output_path)?)
-            };
+                if !lstmf_path.exists() {
+                    prepare_lstmf(stem, &box_orig_path, &output_path_stem)?;
+                }
 
-            eyre::Ok((lstmf_path, output))
-        })
-        .collect::<Result<Vec<_>, _>>()?;
+                let output = generate_lstmf_cmd(
+                    &output_path_stem,
+                    output_path_stem.with_extension("tif"),
+                    PageSegMode::SingleColumn,
+                )
+                .output()?;
 
-    let mut list_file = BufWriter::new(File::create(out_dir.join("list.txt"))?);
-    for (lstmf_path, output) in files {
-        let Some(output) = output else {
-            println!("{} {lstmf_path:?}", "Skipped".yellow());
-            continue;
-        };
+                eyre::Ok((lstmf_path, output))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        outs.into_iter()
+            .map(|(path, output)| {
+                if output.status.success() {
+                    println!("{} {path:?}", "Generated".blue());
+                } else {
+                    println!("{} {path:?}", "Failed".red());
+                }
 
-        if output.status.success() {
-            println!("{} {lstmf_path:?}", "Generated".blue());
-        } else {
-            println!("{} {lstmf_path:?}", "Failed".red());
-        }
+                // print output to current process
+                stdout().write_all(output.stdout.as_slice())?;
+                stdout().flush()?;
+                stderr().write_all(output.stderr.as_slice())?;
+                stderr().flush()?;
+                eyre::Ok(path)
+            })
+            .collect::<Result<Vec<_>, _>>()?
+    };
 
-        // print output to current process
-        stdout().write_all(output.stdout.as_slice())?;
-        stdout().flush()?;
-        stderr().write_all(output.stderr.as_slice())?;
-        stderr().flush()?;
-
+    let mut list_file =
+        BufWriter::new(File::options().append(true).create(true).open(out_dir.join("list.txt"))?);
+    for lstmf_path in files {
         list_file
             .write_all(lstmf_path.file_name().ok_or_eyre("no file name")?.as_encoded_bytes())?;
         writeln!(list_file)?;
@@ -70,11 +118,7 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-fn generate_lstmf(
-    stem: &OsStr,
-    box_orig_path: &Path,
-    output_path_stem: &Path,
-) -> eyre::Result<Output> {
+fn prepare_lstmf(stem: &OsStr, box_orig_path: &Path, output_path_stem: &Path) -> eyre::Result<()> {
     let box_path = output_path_stem.with_extension("box");
     if !box_path.exists() {
         fs::copy(box_orig_path, &box_path)?;
@@ -104,13 +148,16 @@ fn generate_lstmf(
         }
     }
 
-    let output = Command::new(tess_dir().join("tesseract"))
-        .args(["-l", "stam"])
+    Ok(())
+}
+
+fn generate_lstmf_cmd(output_path_stem: &Path, tif_path: PathBuf, psm: PageSegMode) -> Command {
+    let mut cmd = Command::new(tess_dir().join("tesseract"));
+    cmd.args(["-l", "stam"])
         .args(["--tessdata-dir", "assets/tessdata"])
+        .args(["--psm", &(psm as u32).to_string()])
         .arg(tif_path)
         .arg(output_path_stem)
-        .arg(tessdata_dir().join("configs/lstm.train"))
-        .output()?;
-
-    Ok(output)
+        .arg(tessdata_dir().join("configs/lstm.train"));
+    cmd
 }
