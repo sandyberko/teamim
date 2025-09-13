e991a7b7395135d83945b4b5d3be5959b7be8fa4
Sandy Berkowitz <sandyberko@outlook.com>
2025-09-13 18:24:09 +0300

    cosmic: that what I meant

--- crates/editor/src/main.rs
+++ crates/editor/src/main.rs
@@ -1,32 +1,63 @@
-mod loading_btn;
 mod strs;
 
 use std::sync::Arc;
 
-use iced::{
-    Element, Task,
-    widget::{Image, button, column, image::Handle, text},
+use cosmic::{
+    Action, Application, Core, Element,
+    app::{Settings, Task},
+    iced::Length,
+    widget::{Column, Image, Row, Space, button, image::Handle, text},
 };
 
-use crate::loading_btn::JobState;
+#[derive(Debug)]
+pub(crate) enum JobState<Ready, Running = ()> {
+    /// the has either not yet started or has already finished.
+    Ready(Result<Ready, Arc<eyre::Report>>),
+    Running(Running),
+}
+
+impl<Ready, Running> JobState<Ready, Running> {
+    fn ready_ok(&self) -> Option<&Ready> {
+        if let JobState::Ready(Ok(ready)) = self { Some(ready) } else { None }
+    }
+}
 
 #[derive(Debug, Clone)]
 enum Message {
     SelectImage,
     ImageLoaded(Result<Option<Handle>, Arc<eyre::Report>>),
+    Draw,
 }
 
+const PADDING: u16 = 5;
+
 struct App {
+    core: Core,
     img: JobState<Option<Handle>>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self { img: JobState::Ready(Ok(None)) }
-    }
-}
+impl Application for App {
+    type Executor = cosmic::executor::Default;
 
-impl App {
+    type Flags = ();
+
+    type Message = Message;
+
+    const APP_ID: &'static str = "org.teamim.editor";
+
+    fn core(&self) -> &cosmic::Core {
+        &self.core
+    }
+
+    fn core_mut(&mut self) -> &mut cosmic::Core {
+        &mut self.core
+    }
+
+    fn init(core: cosmic::Core, _flags: Self::Flags) -> (Self, Task<Message>) {
+        let app = App { core, img: JobState::Ready(Ok(None)) };
+        (app, Task::none())
+    }
+
     fn update(&mut self, msg: Message) -> Task<Message> {
         match msg {
             Message::SelectImage => {
@@ -35,26 +66,93 @@ impl App {
                 }
 
                 self.img = JobState::Running(());
-                Task::perform(select_image(), |res| Message::ImageLoaded(res.map_err(Arc::new)))
+                Task::perform(select_image(), |res| {
+                    Action::App(Message::ImageLoaded(res.map_err(Arc::new)))
+                })
             }
             Message::ImageLoaded(image) => {
                 self.img = JobState::Ready(image);
                 Task::none()
             }
+            Message::Draw => {
+                // [TODO]
+                Task::none()
+            }
         }
     }
     fn view(&'_ self) -> Element<'_, Message> {
-        let select_btn: Element<Message> =
-            button(strs::SELECT_IMG).on_press(Message::SelectImage).into();
+        let toolbar = Row::with_children([
+            button::suggested(strs::SELECT_IMG).on_press(Message::SelectImage).into(),
+            self.img
+                .ready_ok()
+                .and_then(Option::as_ref)
+                .map_or(/* [HACK] */ Space::with_width(0).into(), |_| {
+                    button::text(strs::DRAW_TEAMIM).on_press(Message::Draw).into()
+                }),
+        ])
+        .spacing(PADDING)
+        .width(Length::Fill)
+        .into();
         let img: Element<Message> = match &self.img {
             JobState::Ready(Ok(Some(img))) => Image::new(img).into(),
             JobState::Ready(Ok(None)) => text(strs::NO_IMG).into(),
-            // TODO
+            // [TODO]
             JobState::Ready(Err(_)) => text("ERROR").into(),
-            // TODO
+            // [TODO]
             JobState::Running(()) => text("Loading...").into(),
         };
-        column![select_btn, img].into()
+        Column::with_children([toolbar, img]).spacing(PADDING).padding(PADDING).into()
+    }
+}
+
+#[allow(unused)]
+mod repro {
+    use cosmic::{
+        Element, Renderer, Theme,
+        iced::{Length, Rectangle, Size},
+        iced_core::{
+            Layout,
+            layout::{Limits, Node},
+            mouse::Cursor,
+            renderer::Style,
+            widget::Tree,
+        },
+        widget::Widget,
+    };
+
+    pub struct Btn<Message>(pub Element<'static, Message>);
+    impl<Message> Widget<Message, Theme, Renderer> for Btn<Message> {
+        fn children(&self) -> Vec<Tree> {
+            vec![Tree::new(&self.0)]
+        }
+        fn size(&self) -> Size<Length> {
+            Size::new(Length::Shrink, Length::Shrink)
+        }
+
+        fn layout(&self, tree: &mut Tree, renderer: &Renderer, limits: &Limits) -> Node {
+            self.0.as_widget().layout(&mut tree.children[0], renderer, limits)
+        }
+
+        fn draw(
+            &self,
+            tree: &Tree,
+            renderer: &mut Renderer,
+            theme: &Theme,
+            style: &Style,
+            layout: Layout<'_>,
+            cursor: Cursor,
+            viewport: &Rectangle,
+        ) {
+            self.0.as_widget().draw(
+                &tree.children[0],
+                renderer,
+                theme,
+                style,
+                layout,
+                cursor,
+                &viewport.intersection(&layout.bounds()).unwrap_or_default(),
+            );
+        }
     }
 }
 
@@ -68,6 +166,7 @@ async fn select_image() -> eyre::Result<Option<Handle>> {
     Ok(Some(Handle::from_path(picked_file.path())))
 }
 
-fn main() -> iced::Result {
-    iced::run("A cool counter", App::update, App::view)
+fn main() -> eyre::Result<()> {
+    cosmic::app::run::<App>(Settings::default(), ())?;
+    Ok(())
 }
--- crates/editor/src/strs.rs
+++ crates/editor/src/strs.rs
@@ -1,2 +1,4 @@
+
+pub const DRAW_TEAMIM: &str = "צייר טעמים";
 pub const NO_IMG: &str = "לא נבחרה תמונה";
 pub const SELECT_IMG: &str = "בחר תמונה";
